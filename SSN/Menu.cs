cd1e6b523e15b84bbac0ad04e36c1bdb063e20d0
carlo <carlo@LAPTOP-7JS1GP07>
2022-05-18 12:47:09 -0400

    Agregar archivos de proyecto.

---
+++ SSN/Menu.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace SSN
+{
+    class Menu
+    {
+        public static bool Show(string VId)
+        {
+            Regex comparator = new Regex(@"^(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}$");
+
+            if (VId == null)
+            {
+                return false;
+            }
+            else
+            {
+                bool matching = comparator.IsMatch(VId);
+                if (matching)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
---
+++ SSN/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SSN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string comparator;
+
+            Console.Write("SSN: ");
+            comparator = Console.ReadLine();
+
+            Console.WriteLine(Menu.Show(comparator));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
